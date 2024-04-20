774f6d093945d47eca0f96750e0b908354c81938
declanl482 <declanl482@gmail.com>
2024-04-19 23:39:11 -0400

    added documentation to problem 0570

--- LEETCODE/SQL/0570_MANAGERS_WITH_AT_LEAST_5_DIRECT_REPORTS.sql
+++ LEETCODE/SQL/0570_MANAGERS_WITH_AT_LEAST_5_DIRECT_REPORTS.sql
@@ -3,7 +3,11 @@
 
 -- We perform an inner self join to create row entries that pair a manager to an
 -- employee that they oversee (this is based on the condition that the employee's
--- manager ID matches the manager's ID). 
+-- manager ID matches the manager's ID).
+
+-- Then, we group employees according to their manager ID and check if the count of
+-- employees in a group associated with a manager ID is at least 5. If so, we select
+-- the name of that manager.
 
 SELECT
     m.name
