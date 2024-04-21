a15bbf45e2d267692ffc66d3a64513848d2cfdfe
declanl482 <declanl482@gmail.com>
2024-04-21 06:16:06 -0400

    added documentation for problem 217

--- LEETCODE/JAVA/0217_CONTAINS_DUPLICATE.java
+++ LEETCODE/JAVA/0217_CONTAINS_DUPLICATE.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> hset = new HashSet<Integer>();
-        
+
+        // Add each element of the array to the hashset until a duplicate is encountered.        
         for (int i = 0; i < nums.length; i++) {
             if (!hset.add(nums[i])) {
                 return true; 
             }
         }
+
         return false;
     }
 }
