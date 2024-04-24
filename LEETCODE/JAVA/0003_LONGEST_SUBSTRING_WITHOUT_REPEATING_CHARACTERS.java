5f902dd68490ca8eb303584cbdbbd9139b0ec57f
declanl482 <declanl482@gmail.com>
2024-04-23 20:15:48 -0400

    added solution for problem 3

---
+++ LEETCODE/JAVA/0003_LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int maxLen = 0;
+        Set<Character> charSet = new HashSet<>();
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left++));
+            }
+            charSet.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
