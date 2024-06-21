234646b9d6b034e5260d06a3a9d136ad9bb1f794
declanl482 <declanl482@gmail.com>
2024-06-20 22:55:33 -0400

    modified solution for problem 21 to be recursive

--- LEETCODE/JAVA/0021_MERGE_TWO_SORTED_LISTS.java
+++ LEETCODE/JAVA/0021_MERGE_TWO_SORTED_LISTS.java
@@ -8,25 +8,22 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+
+ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode sentinel = new ListNode(0);
-        ListNode cur = sentinel;
-        
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                cur.next = list1;
-                list1 = list1.next;
-            }
-            else {
-                cur.next = list2;
-                list2 = list2.next;
-            }
-            cur = cur.next;
+        // If one list is null, return the other.
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            // If the value of list1 head > value of list2 head, recursively merge the remainder of list1 with list2.
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
-        
-        cur.next = (list1 == null) ? list2 : list1;
-        
-        return sentinel.next;
+        else {
+            // If the value of list1 head <= value of list2 head, recursively merge the remainder of list2 with list1.
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
