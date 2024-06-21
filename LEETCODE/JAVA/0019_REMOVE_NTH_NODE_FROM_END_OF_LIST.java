236129b90ad8745fa5f0fb940205a29fd0a70f03
declanl482 <declanl482@gmail.com>
2024-06-21 00:15:52 -0400

    updated solution for problem 19

--- LEETCODE/JAVA/0019_REMOVE_NTH_NODE_FROM_END_OF_LIST.java
+++ LEETCODE/JAVA/0019_REMOVE_NTH_NODE_FROM_END_OF_LIST.java
@@ -8,29 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        // Move the fast pointer n positions from the head.
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
+
+        // Attach a dummy node to the head of the linked list.
+        ListNode dummy = new ListNode(0, head);
+
+        // Maintain left and right pointers on the list.
+        ListNode left = dummy;
+        ListNode right = head;
+
+        // Advance the right pointer n nodes ahead.
+        // Since n <= size, we are left with size - n ( >= 0 ) number of nodes to move the right and left
+        // pointers. 
+        while (n > 0) {
+            right = right.next;
+            --n;
         }
-        
-        // Check if the list contained a single node.
-        if (fast == null) {
-            return head.next;
+
+        // Advance the left and right pointer until the right pointer becomes null.
+        // The left pointer should now point to the node prior to the node to be removed.
+        while (right != null) {
+            left = left.next;
+            right = right.next;
         }
-        
-        // Loop while the fast pointer does not point to the tail node.
-        while (fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        
-        slow.next = slow.next.next;
-        
-        return head;
+
+        // Delete the pointer following the left node and update the next link of the left node.
+        left.next = left.next.next;
+
+        // Return the original head of the linked list.
+        return dummy.next;
     }
 }
