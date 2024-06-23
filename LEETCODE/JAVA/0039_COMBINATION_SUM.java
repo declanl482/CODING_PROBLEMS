ccb8becd526cc890dd25d96d1a046cba06bc5218
declanl482 <declanl482@gmail.com>
2024-06-22 23:23:37 -0400

    added documentation for problem 39

--- LEETCODE/JAVA/0039_COMBINATION_SUM.java
+++ LEETCODE/JAVA/0039_COMBINATION_SUM.java
@@ -7,15 +7,18 @@ class Solution {
     }
 
     private void backtrack(int[] nums, int target, int i, List<List<Integer>> res, List<Integer> combination) {
+        // If the current combination sum exceeds the target, stop exploring this branch.
         if (target < 0) {
             return;
         }
 
+        // If the current combination sum equals the target, add the combination to the result list and stop exploring this branch.        
         if (target == 0) {
             res.add(new ArrayList<>(combination));
             return;
         }
 
+        // If the current combination sum is less than the target, recursively explore the other branches of the combination sum while backtracking.
         for (int j = i; j < nums.length; j++) {
             combination.add(nums[j]);
             backtrack(nums, target - nums[j], j, res, combination);
