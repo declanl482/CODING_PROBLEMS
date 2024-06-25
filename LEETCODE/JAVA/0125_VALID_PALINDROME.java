89d656bd800becc52669b88c9c11f89185d2cbad
declanl482 <declanl482@gmail.com>
2024-06-24 22:48:46 -0400

    added documentation for problem 125

--- LEETCODE/JAVA/0125_VALID_PALINDROME.java
+++ LEETCODE/JAVA/0125_VALID_PALINDROME.java
@@ -4,13 +4,16 @@ class Solution {
         int right = s.length() - 1;
         
         while (left <= right) {
+            // Remove non-alphanumeric characters at the left side.
             if (!Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
             else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                // Remove non-alphanumeric characters at the right side.
                 right--;
             }
             else {
+                // Check that alphanumeric characters at each end are equal (case-insensitive).
                 if (Character.toLowerCase(s.charAt(left++)) != Character.toLowerCase(s.charAt(right--))) {
                     return false;
                 }
