bb4a2a62d8bbb6bec25d79fdd339d3d111b88081
declanl482 <declanl482@gmail.com>
2024-06-26 21:31:52 -0400

    added documentation for problem 141

--- LEETCODE/JAVA/0141_LINKED_LIST_CYCLE.java
+++ LEETCODE/JAVA/0141_LINKED_LIST_CYCLE.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-public class Solution {
+class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
@@ -17,10 +17,14 @@ public class Solution {
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            // If there is an overlap between the fast and slow pointer, a cycle has been detected.
+            // This is akin to having a slower and faster runner on a circular track. The faster runner will lap the slower runner eventually.
             if (slow == fast) {
                 return true;
             }
         }
+
+        // If there is no cycle, the fast pointer should reach the end of the list (be it the tail or the null point) before the slow pointer.
         return false;
     }
 }
