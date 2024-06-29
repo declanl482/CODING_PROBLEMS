6c8bf2208a64c714c33e5c9120510c9a59d9ae60
declanl482 <declanl482@gmail.com>
2024-06-28 23:43:17 -0400

    added documentation to problem 53

--- LEETCODE/JAVA/0053_MAXIMUM_SUBARRAY.java
+++ LEETCODE/JAVA/0053_MAXIMUM_SUBARRAY.java
@@ -1,17 +1,26 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // Implement Kadane's algorithm to compute subarray sums.
-        // Time Complexity: O(n)
-        
-        int currentSum = 0;
-        int maxSum = nums[0];
-        
-        for (int i = 0; i < nums.length; i++) {
-            currentSum += nums[i];
-            maxSum = maxSum < currentSum ? currentSum : maxSum;
-            currentSum = currentSum < 0 ? 0 : currentSum;
+
+        // Implementing an algorithm known as Kadane's algorithm.
+
+        // Initialize the maximum subarray sum as the value of the first element in the input array.
+        int max = nums[0];
+        int sum = 0;
+
+        // Track the running subarray sum over the entire array.
+        for (int num : nums) {
+            // Compute the sum of the current subarray and maximize the result.
+            sum += num;
+            max = Math.max(max, sum);
+
+            // If the sum of the current subarray becomes negative, reset the subarray by resetting the sum to 0.
+            // This is a greedy method of maximizing the subarray sum. For negative values, they will be maximized by
+            // the Math.max call above if the maximum subarray sum happens to be negative.
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-        
-        return maxSum;
+
+        return max;
     }
 }
