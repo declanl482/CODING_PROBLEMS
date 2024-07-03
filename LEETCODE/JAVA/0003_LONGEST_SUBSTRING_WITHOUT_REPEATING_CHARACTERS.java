3e6a1292377fbec65c37a33b868ecf2ee5d5bedc
declanl482 <declanl482@gmail.com>
2024-07-02 23:25:07 -0400

    added documentation for problem 3

--- LEETCODE/JAVA/0003_LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS.java
+++ LEETCODE/JAVA/0003_LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS.java
@@ -4,10 +4,15 @@ class Solution {
         int maxLen = 0;
         Set<Character> charSet = new HashSet<>();
         for (int right = 0; right < s.length(); right++) {
+            // shrink the window while there IS a repeating character in the current window substring
             while (charSet.contains(s.charAt(right))) {
                 charSet.remove(s.charAt(left++));
             }
+
+            // advance the window for the current character
             charSet.add(s.charAt(right));
+
+            // maximize the length
             maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
